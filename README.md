7ad11dccb91d6388c0641c2519149116bbb9bf01
Ravindra Bhanderi <ravibhanderi14@gmail.com>
2018-08-22 16:15:08 +0530

    document

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 A new Flutter package for Byutifull navigation
 
+# Why develop this package
+
+A flutter have a packe name a fluro. Fluro is a nice package and easy to use but they have some limitation. If i need to pass a parameter in Map, List type variable that not allow me. Fluro allow only string but in real time thats not good.
+
+App development its diff from web development in web development we need to pass only small amount of data like id, slug or etc but in App development we need to pass data  between screen is object , array or etc.
+
 ## Getting Started
 
-For help getting started with Flutter, view our online [documentation](https://flutter.io/).
 
-For help on editing package code, view the [documentation](https://flutter.io/developing-packages/).
--- lib/navigate.dart
+++ lib/navigate.dart
@@ -1,88 +1,120 @@
 library navigate;
 
-
+import 'dart:async';
 import 'dart:core';
 import 'package:flutter/material.dart';
 
-/* navigation type */
+/* ChangePage type */
 class Navigate {
-
   static final Navigate _instance = Navigate.internal();
   Navigate.internal();
   factory Navigate() => _instance;
-  /* defualt navigation Transactiontype */
-  static TransactionType defultTransactionType =  TransactionType.fromBottom;
+  /* defualt Navigate Transactiontype */
+  static TransactionType defultTransactionType;
   /* routes information */
-  static Map<String,Handler> appRoutes ;
+  static Map<String, Handler> _appRoutes;
 
-  Navigate.navigate(BuildContext context,String routeName,arg,[TransactionType transactionType]){
-
-    if(appRoutes.containsKey(routeName)) {
-
-      var handler = appRoutes[routeName];
-      handler.renderWithAnimation(context,arg,transactionType);
+  static Future<dynamic> navigate(BuildContext context, String routeName,{arg,
+      TransactionType transactionType,
+      ReplaceRoute replaceRoute = ReplaceRoute.none}) async {
+    if (_appRoutes.containsKey(routeName)) {
+      var handler = _appRoutes[routeName];
+      return await handler.renderWithAnimation(
+          context, arg, transactionType, replaceRoute);
+    } else {
+      throw ("ROUTE NOT MATCH");
     }
   }
 
-  registerRoutes(Map<String,Handler> routes) {
-
-    appRoutes = routes;
+  static registerRoutes(
+      {Map<String, Handler> routes,
+      TransactionType defualtTransactionType = TransactionType.fromBottom}) {
+    _appRoutes = routes;
+    defultTransactionType = defualtTransactionType;
   }
 }
 
-/* Route navigation handler */
+/* Route ChangePage handler */
 class Handler {
-
-  final  Function(BuildContext context,dynamic arg) pageBuilder;
+  final Function(BuildContext context, dynamic arg) pageBuilder;
   TransactionType transactionType;
-  Handler({this.pageBuilder,this.transactionType});
+  Handler({this.pageBuilder, this.transactionType});
 
-  renderWithAnimation(BuildContext context, arg ,transactionType){
-    Navigator.push(
-        context,
-        new PageRouteBuilder(
-            opaque: false,
-            pageBuilder: (BuildContext context, _, __) {
-              return pageBuilder(context,arg);
-            },
-            transitionsBuilder: PageTransaction(transactionType)));
+  Future<dynamic> renderWithAnimation(BuildContext context, arg,
+      transactionType, ReplaceRoute replaceRoute) async {
+    if (replaceRoute == ReplaceRoute.none) {
+      return await Navigator.push(
+          context,
+          new PageRouteBuilder(
+              opaque: false,
+              pageBuilder: (BuildContext context, _, __) {
+                return pageBuilder(context, arg);
+              },
+              transitionsBuilder: pageTransaction(transactionType)));
+    } else if (replaceRoute == ReplaceRoute.thisOne) {
+      return await Navigator.pushReplacement(
+          context,
+          new PageRouteBuilder(
+              opaque: false,
+              pageBuilder: (BuildContext context, _, __) {
+                return pageBuilder(context, arg);
+              },
+              transitionsBuilder: pageTransaction(transactionType)));
+    } else if (replaceRoute == ReplaceRoute.all) {
+      return await Navigator.pushAndRemoveUntil(
+          context,
+          new PageRouteBuilder(
+              opaque: false,
+              pageBuilder: (BuildContext context, _, __) {
+                return pageBuilder(context, arg);
+              },
+              transitionsBuilder: pageTransaction(transactionType)),
+          (Route<dynamic> route) => false);
+    }
   }
 
-  PageTransaction(TransactionType comeFromParam) {
+  pageTransaction(TransactionType comeFromParam) {
+    TransactionType pageTras = comeFromParam != null
+        ? comeFromParam
+        : (this.transactionType != null)
+            ? this.transactionType
+            : Navigate.defultTransactionType;
 
-     TransactionType pageTras = comeFromParam != null
-                                                      ? comeFromParam
-                                                      : (this.transactionType != null) ? this.transactionType : Navigate.defultTransactionType;
+    if (pageTras != TransactionType.fadeIn) {
+      double x = 0.0;
+      double y = 0.0;
 
-     if(pageTras != TransactionType.fadeIn) {
-        double x = 0.0;
-        double y = 0.0;
+      switch (pageTras) {
+        case TransactionType.fromBottom:
+          y = 1.0;
+          break;
+        case TransactionType.fromTop:
+          y = -1.0;
+          break;
+        case TransactionType.fromLeft:
+          x = -1.0;
+          break;
+        case TransactionType.fromRight:
+          x = 1.0;
+          break;
+      }
 
-        switch(pageTras){
-
-          case TransactionType.fromBottom: y = 1.0;
-                                            break;
-        }
-
-        return (___, Animation<double> animation, ____, Widget child) {
-          return new SlideTransition(
-            position: Tween(
-               begin: Offset(x, y),
-               end:Offset(0.0,0.0)
-            ).animate(animation),
-            child: child,
-          );
-        };
-
-     }else {
-        return (___, Animation<double> animation, ____, Widget child) {
-         return new FadeTransition(
-           opacity: animation,
-           child: child,
-         );
-       };
-     }
+      return (___, Animation<double> animation, ____, Widget child) {
+        return new SlideTransition(
+          position: Tween(begin: Offset(x, y), end: Offset(0.0, 0.0))
+              .animate(animation),
+          child: child,
+        );
+      };
+    } else {
+      return (___, Animation<double> animation, ____, Widget child) {
+        return new FadeTransition(
+          opacity: animation,
+          child: child,
+        );
+      };
     }
+  }
 }
 
 /* enum of page translation type */
@@ -94,3 +126,5 @@ enum TransactionType {
   fadeIn,
   custom,
 }
+/* replacement type enum */
+enum ReplaceRoute { thisOne, all, none }
